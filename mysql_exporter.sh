c6584cd69b5fb0d99b455ff0ecc8acd6f32bd22f
Nishant Kumar <nishant.kumar@edvanta.com>
2022-02-09 14:16:21 +0000

    Updated mysql files

--- mysql_exporter.sh
+++ mysql_exporter.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
 
+curl -s https://api.github.com/repos/prometheus/mysqld_exporter/releases/latest | grep browser_download_url | grep linux-amd64 | cut -d '"' -f 4 | wget -qi -
+tar xvf mysqld_exporter-*.linux-amd64.tar.gz
+cd mysqld_exporter-*.linux-amd64/
+chmod +x mysqld_exporter
+sudo mv mysqld_exporter /usr/local/bin
+
 #TODO: Write the actual commands
 read -p 'Enter admin user for mysql: ' -r ADMIN_USER
 read -sp 'Enter the password for admin user: ' -r ADMIN_PASS
 read -sp 'Enter the password for mysqld_exporter user' -r MYSQLD_PASS
 
 #TODO: Find how to take password from user input when running the script.
-echo "CREATE USER 'mysqld_exporter'@'localhost' IDENTIFIED BY ${MYSQLD_PASS} WITH MAX_USER_CONNECTIONS 3;
+echo "CREATE USER 'mysqld_exporter'@'localhost' IDENTIFIED BY \"${MYSQLD_PASS}\" WITH MAX_USER_CONNECTIONS 3;
 GRANT PROCESS, REPLICATION CLIENT, SELECT ON *.* TO 'mysqld_exporter'@'localhost';
 FLUSH PRIVILEGES;" >sql_create_user.sql
 
@@ -17,6 +23,7 @@ user=mysqld_exporter
 password=${MYSQLD_PASS}
 " >/etc/.mysqld_exporter.cnf
 
+groupadd prometheus
 chown root:prometheus /etc/.mysqld_exporter.cnf
 
 useradd -rs /bin/false mysqld_exporter
@@ -51,4 +58,4 @@ ExecStart=/usr/local/bin/mysqld_exporter \\
 --web.listen-address=0.0.0.0:9104
 
 [Install]
-WantedBy=multi-user.target" >/etc/system/systemd/mysqld_exporter.service
+WantedBy=multi-user.target" >/etc/systemd/system/mysqld_exporter.service
